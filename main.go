d1d246c3b0f3950724fc6de0a8940437dcffac3d
Tyler Mandry <tmandry@gmail.com>
2013-09-20 02:24:32 -0500

    Refactor human vs random

--- main.go
+++ main.go
@@ -8,13 +8,25 @@ import (
 
 func main() {
 	game := pg.NewGame()
-	play(game)
+	playHvR(&game)
 }
 
-func play(game pg.Game) {
-	directions := map[int]string{pg.Clockwise: "CW", pg.CounterClockwise: "CCW"}
-	colors := map[pg.Piece]string{pg.White: "White", pg.Black: "Black"}
+var colors = map[pg.Piece]string{pg.White: "White", pg.Black: "Black"}
 
+func playHvH(game *pg.Game) {
+	for {
+		fmt.Print(game.Board)
+		if winner := game.CheckWinner(); winner != pg.Empty {
+			fmt.Printf("%s won!", colors[winner])
+			break
+		}
+		move := promptForMove(game)
+		game.Move(move)
+		fmt.Println(move)
+	}
+}
+
+func playHvR(game *pg.Game) {
 	for {
 		fmt.Print(game.Board)
 		if winner := game.CheckWinner(); winner != pg.Empty {
@@ -22,28 +34,42 @@ func play(game pg.Game) {
 			break
 		}
 
-		var r, c, sub, dir int
-		for ok := false; !ok; {
-			fmt.Printf("%s> ", colors[game.Turn])
-
-			_, err := fmt.Scanf("%d %d %d %d", &r, &c, &sub, &dir)
-			switch {
-			case err != nil:
-				fmt.Println("Invalid format")
-			case r < 0 || r >= 6 || c < 0 || c >= 6:
-				fmt.Println("Row and columns are 0-5")
-			case sub < 0 || sub >= 4:
-				fmt.Println("Subboard is 0-3")
-			case !(dir == 0 || dir == 1):
-				fmt.Println("Direction is 0 for clockwise, 1 for counterclockwise")
-			default:
-				ok = game.Move(r, c, sub, dir)
-				if !ok {
-					fmt.Println("Invalid move")
-				}
-			}
+		var move pg.Move
+		if game.Turn == pg.White {
+			move = promptForMove(game)
+		} else {
+			move = game.Board.RandomMove()
 		}
 
-		fmt.Printf("Put piece (%d, %d), rotate subboard %d %s\n", r, c, sub, directions[dir])
+		if ok := game.Move(move); !ok {
+			fmt.Println("Error")
+		}
+		fmt.Println(move)
 	}
+}
+
+func promptForMove(game *pg.Game) (move pg.Move) {
+	var r, c, sub, dir int
+	for ok := false; !ok; {
+		fmt.Printf("%s> ", colors[game.Turn])
+
+		_, err := fmt.Scanf("%d %d %d %d", &r, &c, &sub, &dir)
+		switch {
+		case err != nil:
+			fmt.Println("Invalid format")
+		case r < 0 || r >= 6 || c < 0 || c >= 6:
+			fmt.Println("Row and columns are 0-5")
+		case sub < 0 || sub >= 4:
+			fmt.Println("Subboard is 0-3")
+		case !(dir == 0 || dir == 1):
+			fmt.Println("Direction is 0 for clockwise, 1 for counterclockwise")
+		default:
+			move = pg.NewMove(r, c, sub, dir)
+			if ok = move.IsValid(game.Board); !ok {
+				fmt.Println("Invalid move")
+			}
+		}
+	}
+
+	return move
 }
